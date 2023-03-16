d02d338d80b7b494e6cebc5a9fa87166edba75f6
ngkao <ng.kao.ca@gmail.com>
2023-03-16 16:21:23 -0400

    add handleSubmit on form

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,11 +1,28 @@
 import "./InputField.scss"
+import { useState } from "react";
 
 const InputField = () => {
+
+    const [input, setInput] = useState();
+    console.log(input)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Send", input)
+    }
+
+
     return (
         <section className="input">
-            <form className='input__form'>
+            <form 
+                className='input__form'
+                onSubmit={handleSubmit}
+            >
                 <label className='input__heading'>Input Field</label>
-                <textarea className='input__box'>
+                <textarea 
+                    onChange={(event) => {setInput(event.target.value)}} 
+                    value={input} 
+                    className='input__box'>
                 </textarea>
                 <button className='input__btn'>START/STOP</button>
             </form>
